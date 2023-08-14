fb5d07652b6c223602ae63f50a4f243d49484251
Ryan Kwon <kznm.develop@gmail.com>
2023-08-14 22:24:05 +0900

    dijkstra -> A*

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from sqlmodel import Session, SQLModel, create_engine, Field
 from typing import Optional
 from pydantic import BaseModel
+from collections import defaultdict
+from heapq import heappush, heappop
+import heapq
 
 
 class Edge(SQLModel, table=True):
@@ -47,10 +50,8 @@ def search_route(loc1: str, loc2: str):
         for edge in edges:
             routes.append((edge.start_point, edge.end_point, edge.move_minutes))
             routes.append((edge.end_point, edge.start_point, edge.move_minutes))
-        # dijkstra
-        from collections import defaultdict
-        import heapq
 
+        # dijkstra
         def dijkstra(edges, f, t):
             g = defaultdict(list)
             for l, r, c in edges:
@@ -68,7 +69,32 @@ def search_route(loc1: str, loc2: str):
                             heapq.heappush(q, (cost + c, v2, path))
             return float("inf")
 
-        return dijkstra(routes, loc1, loc2)
+        # A*
+        def astar(graph, start, goal):
+
+            graph = defaultdict(list)
+            for start, end, weight in routes:
+                graph[start].append((weight, end))
+
+            def astar_search(graph, start, goal):
+                visited = set()
+                queue = []
+                heappush(queue, (0, start, [start]))
+                while queue:
+                    (cost, node, path) = heappop(queue)
+                    if node not in visited:
+                        visited.add(node)
+                        if node == goal:
+                            return cost, path
+                        for weight, neighbour in graph[node]:
+                            if neighbour not in visited:
+                                heappush(queue, (cost + weight, neighbour, path + [neighbour]))
+                return 0, []
+
+            return astar_search(graph, start, goal)
+
+        # return dijkstra(routes, loc1, loc2)
+        return astar(routes, loc1, loc2)
 
 
 @app.delete("/edges/{edge_id}")
